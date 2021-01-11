17a3493949eae17408ea3d3454a7e6363952afb4
Manuel Carrizo <manuelcarrizo@gmail.com>
2021-01-10 21:36:36 -0300

    added optional category to resulting xml

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ with open("config.json", "r") as config_file:
     config = json.load(config_file)
 
 for feed in config["feeds"]:
-    print(feed["url"])
-
     with request.urlopen(feed["url"], timeout=10) as f:
         data = f.read().decode('utf-8')
 
@@ -26,11 +24,17 @@ for feed in config["feeds"]:
                 some_matches = all(map(lambda w: w in title, tuple(filters["keys"])))
 
                 if some_matches:
+                    if filters.get("category", None):
+                        elem = '<qbCategory>%s</qbCategory>' % filters["category"]
+                        item.append((ElementTree.fromstring(elem)))
                     break
 
             if not some_matches:
                 channel.remove(item)
 
-        with open(feed["output"], "w") as f:
-            f.write(ElementTree.tostring(et).decode('utf-8'))
+        if feed["output"] == "stdout":
+            print(ElementTree.tostring(et).decode('utf-8'))
+        else:
+            with open(feed["output"], "w") as f:
+                f.write(ElementTree.tostring(et).decode('utf-8'))
 
